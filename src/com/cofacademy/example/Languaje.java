69f8201050cdfe6e172bf9b421fc3efd7fefb029
Ivan Rubio <smashedpurse@gmail.com>
2022-12-29 23:51:53 -0600

    Develope the default method in a interface and use this one into the main class and see the difference

--- src/com/cofacademy/example/Languaje.java
+++ src/com/cofacademy/example/Languaje.java
@@ -2,4 +2,8 @@ package com.cofacademy.example;
 
 public interface Languaje  {
     void sayHi();
+
+    default void sayBye(){
+        System.out.println("Say");
+    }
 }
--- src/com/cofacademy/example/Main.java
+++ src/com/cofacademy/example/Main.java
@@ -3,7 +3,7 @@ package com.cofacademy.example;
 public class Main {
     public static void main(String[] args) {
 
-        
+
         EnglishLanguaje e = new EnglishLanguaje();
         e.sayHi();
         ProgammingLanguaje p = new ProgammingLanguaje();
